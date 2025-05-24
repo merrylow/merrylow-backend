330b8f8c57499b75f93bb15ed5863cd80b86eb64
ZiglaCity <ziglacity@gmail.com>
2025-05-24 16:03:47 +0000

    Extend token expiration

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -5,8 +5,8 @@ const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET || "access_secret_key";
 const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET || "refresh_secret_key";
 
 const generateTokens = (id, userRole = "CUSTOMER", email) => {
-    const accessToken =  jwt.sign({id, userRole, email}, ACCESS_SECRET, {expiresIn: "1h"} )
-    const refreshToken = jwt.sign({id, userRole, email}, REFRESH_SECRET, {expiresIn: "3d"});
+    const accessToken =  jwt.sign({id, userRole, email}, ACCESS_SECRET, {expiresIn: "1d"} )
+    const refreshToken = jwt.sign({id, userRole, email}, REFRESH_SECRET, {expiresIn: "7d"});
 
     return {accessToken, refreshToken}
 }
